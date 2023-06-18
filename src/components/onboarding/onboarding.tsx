f053114bbd0758abd8d1467f73a881d481341c8b
Vincent Chan <okcdz@diverse.space>
2023-06-19 00:12:26 +0800

    feat: privacy messages to onboarding

--- src/components/onboarding/onboarding.tsx
+++ src/components/onboarding/onboarding.tsx
@@ -6,6 +6,28 @@ import { AppStatus } from "@pkg/models/app_state";
 import { PrimaryButton } from "@pkg/components/button";
 import Face from "./face.svg?url";
 
+interface PrettyCheckBoxProps {
+  content: string;
+  description: string;
+}
+
+function PrettyCheckBox(props: PrettyCheckBoxProps) {
+  const { content, description } = props;
+  const [checked, setChecked] = useState(true);
+  const handleClick = () => {
+    setChecked(!checked);
+  }
+  return (
+    <div className={classes.prettyCheckBox} onClick={handleClick}>
+      <input type="checkbox" checked={checked} />
+      <div className={classes.mainLine}>
+        <p className="content">{content}</p>
+        <p className="description">{description}</p>
+      </div>
+    </div>
+  );
+}
+
 export const Onboarding = memo(() => {
   const appState = useContext(AppContext)!;
   const [showAni, setShowAni] = useState(false);
@@ -41,6 +63,16 @@ export const Onboarding = memo(() => {
           <br />
           Press the button below to install the script.
         </p>
+        <div className={classes.checkboxContainer}>
+          <PrettyCheckBox
+            content="Allow Terminal One to collect anonymous usage data."
+            description="Help us improve Terminal One. We cannot track your identity from the collected data."
+          />
+          <PrettyCheckBox
+            content="Allow Terminal One to collect diagnostic and performance data."
+            description="Help us improve the performance and stability of Terminal One. We cannot track your identity from the collected data."
+          />
+        </div>
         <PrimaryButton onClick={handleStart}>
           Install and Get started
         </PrimaryButton>
