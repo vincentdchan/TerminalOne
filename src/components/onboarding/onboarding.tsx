4acbe5fa33afad8a4e80e22ba489541a56b2f7d6
Vincent Chan <okcdz@diverse.space>
2023-06-18 22:32:59 +0800

    feat: add face mark to onboarding

--- src/components/onboarding/onboarding.tsx
+++ src/components/onboarding/onboarding.tsx
@@ -2,6 +2,8 @@ import { useContext, useCallback, memo } from "react";
 import classes from "./onboarding.module.css";
 import { AppContext } from "@pkg/contexts/app_context";
 import { AppStatus } from "@pkg/models/app_state";
+import { PrimaryButton } from "@pkg/components/button";
+import Face from "./face.svg?url";
 
 export const Onboarding = memo(() => {
   const appState = useContext(AppContext)!;
@@ -17,14 +19,17 @@ export const Onboarding = memo(() => {
         {...({ "data-tauri-drag-region": true } as any)}
       ></div>
       <div className={classes.content}>
-        <h1 className="t1-noselect">Welcome</h1>
+        <div className={classes.header}>
+          <img src={Face} />
+          <h1 className="t1-noselect">Welcome</h1>
+        </div>
         <p>
           Terminal One needs to install a
           script to your <i>.zshrc</i> file to work properly.
           <br />
           Press the button below to install the script.
         </p>
-        <button onClick={handleStart}>Install and Get started</button>
+        <PrimaryButton onClick={handleStart}>Install and Get started</PrimaryButton>
       </div>
     </div>
   );
